8d2c87896c892ad558d625b4c12d37ad5b634d84
bhavanishankar7075 <bhavanimandala2004@gmail.com>
2025-11-27 18:33:51 +0530

    designed for the full width taking and responsiveness design

--- shoppyglobe/src/components/Header/Header.jsx
+++ shoppyglobe/src/components/Header/Header.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { selectCartItemCount } from "../../redux/cartSlice";
-import "./Header.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectCartItemCount } from '../../redux/cartSlice';
+import './Header.css';
 
 /**
  * @function Header
  * @description Displays the main navigation and cart icon, connected to Redux.
  */
 function Header() {
-  // Get live cart item count (Requirement: Redux 50 Marks)
-  const cartItemCount = useSelector(selectCartItemCount);
+  // Get live cart item count
+  const cartItemCount = useSelector(selectCartItemCount); 
 
   return (
     <header className="header">
@@ -18,21 +18,18 @@ function Header() {
         <Link to="/">🛍️ ShoppyGlobe</Link>
       </div>
       <nav className="nav-menu">
-        <Link to="/" className="nav-link">
-          Home
-        </Link>
+        <Link to="/" className="nav-link">Home</Link>
         <Link to="/cart" className="nav-link cart-icon-link">
-          🛒 Cart
+          🛒 Cart 
           {/* Display cart count badge if items exist */}
           {cartItemCount > 0 && (
             <span className="cart-count">{cartItemCount}</span>
           )}
         </Link>
-        <Link to="/checkout" className="nav-link checkout-link">
-          Checkout
-        </Link>
+        <Link to="/checkout" className="nav-link checkout-link">Checkout</Link>
       </nav>
     </header>
   );
 }
+
 export default Header;
